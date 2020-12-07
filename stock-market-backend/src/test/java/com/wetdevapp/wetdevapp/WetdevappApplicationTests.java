617a531d6919824b2f88a0fa82bcd2d6939e4f0d
Austin Pelto <austinpelto16@gmail.com>
2020-12-07 16:37:52 -0500

    Angular and Springboot folders

---
+++ stock-market-backend/src/test/java/com/wetdevapp/wetdevapp/WetdevappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wetdevapp.wetdevapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WetdevappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
