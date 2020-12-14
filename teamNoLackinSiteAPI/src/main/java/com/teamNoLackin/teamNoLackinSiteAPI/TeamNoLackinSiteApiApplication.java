bf6c32c24cad597cf1b6de7957baefcbb7071c0c
Austin Pelto <austinpelto16@gmail.com>
2020-12-14 11:50:00 -0500

    New Spring API project

---
+++ teamNoLackinSiteAPI/src/main/java/com/teamNoLackin/teamNoLackinSiteAPI/TeamNoLackinSiteApiApplication.java
@@ -0,0 +1,13 @@
+package com.teamNoLackin.teamNoLackinSiteAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamNoLackinSiteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamNoLackinSiteApiApplication.class, args);
+	}
+
+}
