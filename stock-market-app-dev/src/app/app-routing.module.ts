f391c335593e5d2b33e74e20163a081a668b0760
Austin Pelto <austinpelto16@gmail.com>
2020-12-14 12:48:59 -0500

    Updates to API and Frontend files

--- stock-market-app-dev/src/app/app-routing.module.ts
+++ stock-market-app-dev/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { StockDataComponent } from './stock-data/stock-data.component';
+import { StockDataComponent } from './pages/stock-data/stock-data.component';
 
 const routes: Routes = [
   { path: 'stock-data', component: StockDataComponent }
--- stock-market-app-dev/src/app/app.module.ts
+++ stock-market-app-dev/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StockDataComponent } from './stock-data/stock-data.component';
+import { StockDataComponent } from './pages/stock-data/stock-data.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { SubscribeComponent } from './pages/subscribe/subscribe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StockDataComponent
+    StockDataComponent,
+    FooterComponent,
+    HeaderComponent,
+    SubscribeComponent
   ],
   imports: [
     BrowserModule,
